e84b1edaa22328b7dd16640bdb5a3193cab017f0
abdelkader.bouadjadja <ak.bouadjadja@gmail.com>
2018-03-02 17:14:02 +0100

    Fix docker php-fpm entrypoint

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 
 sleep 5
 php /var/www/symfony/bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
+php /var/www/symfony/bin/console app:match:reindex
+
+onchange.sh php /var/www/symfony/bin/console app:match:reindex
 
 exec "$@"
